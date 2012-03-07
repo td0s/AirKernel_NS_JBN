702ee58c688c313a8aa11b1e6b64add1eef0a0c9
Byeonggon <bluesea1313@naver.com>
2012-03-07 19:17:52 +0900

    [MOD] Add Lower/Higher Frequency Limit on LiveOC
    Conflicts:
    
    	drivers/misc/live_oc.c
    
    Conflicts:
    
    	drivers/misc/live_oc.c

--- drivers/misc/live_oc.c
+++ drivers/misc/live_oc.c
@@ -15,16 +15,28 @@
 
 #define MAX_OCVALUE 150
 
-extern void liveoc_update(unsigned int oc_value, unsigned int oc_target);
+extern void liveoc_update(unsigned int oc_value, unsigned int oc_low_freq, unsigned int oc_high_freq);
 
 static int oc_value = 100;
 
 /* Apply Live OC to 800MHz and above*/
-static int oc_target = 800000;
+static int oc_low_freq = 800000;
+/* Apply Live OC to 2000MHz and below */
+static int oc_high_freq = 2000000;
 
 static ssize_t liveoc_ocvalue_read(struct device * dev, struct device_attribute * attr, char * buf)
 {
     return sprintf(buf, "%u\n", oc_value);
+}
+
+static ssize_t liveoc_octarget_low_read(struct device * dev, struct device_attribute * attr, char * buf)
+{
+    return sprintf(buf, "%u\n", oc_low_freq);
+}
+
+static ssize_t liveoc_octarget_high_read(struct device * dev, struct device_attribute * attr, char * buf)
+{
+    return sprintf(buf, "%u\n", oc_high_freq);
 }
 
 static ssize_t liveoc_ocvalue_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
@@ -39,7 +51,7 @@ static ssize_t liveoc_ocvalue_write(struct device * dev, struct device_attribute
 			{
 			    oc_value = data;
 
-			    liveoc_update(oc_value, oc_target);
+			    liveoc_update(oc_value, oc_low_freq, oc_high_freq);
 			}
 
 		    pr_info("LIVEOC oc-value set to %u\n", oc_value);
@@ -57,17 +69,89 @@ static ssize_t liveoc_ocvalue_write(struct device * dev, struct device_attribute
     return size;
 }
 
+<<<<<<< HEAD
+=======
+static ssize_t liveoc_octarget_low_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+    unsigned int data;
+
+    if(sscanf(buf, "%u\n", &data) == 1)
+	{
+	    if (data != oc_low_freq && data <= oc_high_freq)
+		{
+<<<<<<< HEAD
+		    oc_target = data;
+
+		    liveoc_update(oc_value, oc_target);
+		    pr_info("LIVEOC oc-target set to %u\n", oc_target);
+=======
+		    oc_low_freq = data;
+	    
+		    liveoc_update(oc_value, oc_low_freq, oc_high_freq);
+
+		    pr_info("LIVEOC oc-target-low set to %u\n", oc_low_freq);
+>>>>>>> 286eb6d... [MOD] Add Lower/Higher Frequency Limit on LiveOC
+		}
+	    else
+		{
+		    pr_info("%s: invalid input range %u\n", __FUNCTION__, oc_low_freq);
+		}
+	}
+    else
+	{
+		pr_info("%s: invalid input\n", __FUNCTION__);
+	}
+    return size;
+}
+
+static ssize_t liveoc_octarget_high_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+    unsigned int data;
+
+    if(sscanf(buf, "%u\n", &data) == 1)
+	{
+	    if (data != oc_high_freq && data >= oc_low_freq)
+		{
+		    oc_high_freq = data;
+	    
+		    liveoc_update(oc_value, oc_low_freq, oc_high_freq);
+
+		    pr_info("LIVEOC oc-target-high set to %u\n", oc_high_freq);
+		}
+	    else
+		{
+		    pr_info("%s: invalid input range %u\n", __FUNCTION__, oc_high_freq);
+		}
+	}
+    else
+	{
+		pr_info("%s: invalid input\n", __FUNCTION__);
+	}
+    return size;
+}
+
+>>>>>>> c21bd7f... [MOD] Add Lower/Higher Frequency Limit on LiveOC
 static ssize_t liveoc_version(struct device * dev, struct device_attribute * attr, char * buf)
 {
     return sprintf(buf, "%u\n", LIVEOC_VERSION);
 }
 
 static DEVICE_ATTR(oc_value, S_IRUGO | S_IWUGO, liveoc_ocvalue_read, liveoc_ocvalue_write);
+<<<<<<< HEAD
+=======
+static DEVICE_ATTR(oc_target_low, S_IRUGO | S_IWUGO, liveoc_octarget_low_read, liveoc_octarget_low_write);
+static DEVICE_ATTR(oc_target_high, S_IRUGO | S_IWUGO, liveoc_octarget_high_read, liveoc_octarget_high_write);
+>>>>>>> c21bd7f... [MOD] Add Lower/Higher Frequency Limit on LiveOC
 static DEVICE_ATTR(version, S_IRUGO , liveoc_version, NULL);
 
 static struct attribute *liveoc_attributes[] = 
     {
 	&dev_attr_oc_value.attr,
+<<<<<<< HEAD
+=======
+	&dev_attr_oc_target_low.attr,
+	&dev_attr_oc_target_high.attr,
+>>>>>>> c21bd7f... [MOD] Add Lower/Higher Frequency Limit on LiveOC
 	&dev_attr_version.attr,
 	NULL
     };
